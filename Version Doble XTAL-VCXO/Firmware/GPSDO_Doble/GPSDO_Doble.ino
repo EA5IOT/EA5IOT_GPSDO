8f5623577a6ba79399785605ab6ae253603219e4
EA5IOT <ea5iot@outlook.com>
2019-12-16 09:18:29 +0100

    Update

--- Version Doble XTAL-VCXO/Firmware/GPSDO_Doble/GPSDO_Doble.ino
+++ Version Doble XTAL-VCXO/Firmware/GPSDO_Doble/GPSDO_Doble.ino
@@ -1,5 +1,22 @@
 // EA5IOT GPSDO DOBLE SMT32f103C8T6 XTAL FREQUENCY PWM CONTROL
 
+/*
+    Copyright (C) 2020
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // PATAS Microcontrolador
 #define Timer3Ch1  PB4                                                          // Señal directa del cristal_1
 #define Timer3Ch2  PB5                                                          // Señal de 1PPS del gps (Activa cuando el GPS está enganchado)
